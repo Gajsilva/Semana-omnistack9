25ace1e374078ce1c30da4957e07d8f2cfcbfd45
Gabriel Silva <86989966+Gajsilva@users.noreply.github.com>
2022-09-08 02:36:04 -0300

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose =require('mongoose')
+const cors = require('cors')
+const path = require('path')
+
 const routes = require('./routes')
 const app = express();
 const porta = 3336
@@ -9,10 +12,13 @@ mongoose.connect('mongodb+srv://semana:semana@semana9.vtlce7c.mongodb.net/?retry
     useUnifiedTopology:true,
 })
 
-app.use(express.json())
+
+app.use(cors())
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'upload')))
 app.use(routes);
 
 
 app.listen(porta, () => {
     console.log(`Servidor na porta ${porta}`)
-} )
+} );
